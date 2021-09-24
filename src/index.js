9c9b8c0e4ffb48e58c8e5db61f3f584cbb806068
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2021-09-24 18:40:12 +0300

    Add layout

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 
 app.all('/', (req, res) => {
-    res.render('index', { layout: false });
+    res.render('index');
 });
 
 app.listen(5000, console.log.bind(console, 'Application is running on http://localhost:5000'));
