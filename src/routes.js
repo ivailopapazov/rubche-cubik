0069f07c80072ad9b63ec79b49442cb91df74f4d
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2021-09-24 20:32:09 +0300

    Add errro page

--- src/routes.js
+++ src/routes.js
@@ -7,5 +7,8 @@ const router = express.Router();
 
 router.use(homeController);
 router.use('/cube', cubeController);
+router.use('*', (req, res) => {
+    res.render('404');
+});
 
 module.exports = router;
