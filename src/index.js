4454f320a97c5e72a260a0f30b6edb4aa79ea780
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2021-09-24 18:50:35 +0300

    Add static asset hostin

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 require('./config/handlebars')(app);
+
+app.use(express.static(path.resolve(__dirname, './public')));
 
 app.all('/', (req, res) => {
     res.render('index');
