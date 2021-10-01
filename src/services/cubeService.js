657ecb2736f1c45409c06330b4fc0a9198a0892f
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2021-10-01 21:21:11 +0300

    Show accessories on details page

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -3,7 +3,7 @@ const Accessory = require('../models/Accessory');
 
 const getAll = () => Cube.find({}).lean();
 
-const getOne = (id) => Cube.findById(id).lean();
+const getOne = (id) => Cube.findById(id).populate('accessories').lean()
 
 const create = (name, description, imageUrl, difficulty) => {
     let cube = new Cube({
