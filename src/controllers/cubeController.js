ade985e02720a5e22e769501f96bca5c26f1092a
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2021-10-01 20:03:57 +0300

    Update details template

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -21,7 +21,7 @@ const createCube = async (req, res) => {
 const cubeDetails = async (req, res) => {
     let cube = await cubeService.getOne(req.params.cubeId);
 
-    res.render('details', { ...cube });
+    res.render('cube/details', { ...cube });
 };
 
 router.get('/create', getCreateCubePage);
